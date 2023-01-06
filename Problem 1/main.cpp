e27417ed2248b33c3ef6766bcfb87b0b6e9dac3e
Christopher Luck <120812392+cluck839@users.noreply.github.com>
2023-01-06 11:48:24 -0500

    Add files via upload

---
+++ Problem 1/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void testNumber(int, int, int);
+
+int main()
+{
+    int testCases;
+    int a;
+    int b;
+    int c;
+    cin >> testCases;
+    for(int i=0;i<testCases;i++)
+    {
+        cin >> a;
+        cin >> b;
+        cin >> c;
+        testNumber(a,b,c);
+    }
+    system("pause");
+    return 0;
+}
+
+void testNumber(int a,int b, int c)
+{
+    if(a + b == c)
+        cout << "Addition" << endl;
+    else if(a - b == c)
+        cout << "Subtraction" << endl;
+    else if(a * b == c)
+        cout << "Multiplication" << endl;
+    else if(a / b == c)
+        cout << "Division" << endl;
+    else if(a % b == c)
+        cout << "Modulo" << endl;
+}
